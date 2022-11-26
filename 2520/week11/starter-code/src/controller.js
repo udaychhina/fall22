805b99903f4d6abbcf6670ce961b3bf64d4d6ade
Uday Chhina <udaychhina1337@gmail.com>
2022-11-25 16:12:20 -0800

    milestone 2 js

--- 2520/week11/starter-code/src/controller.js
+++ 2520/week11/starter-code/src/controller.js
@@ -1,16 +1,19 @@
 const fs = require("fs").promises;
-const { createReadStream } = require("fs")
-const { DEFAULT_HEADER } = require("./util/util");
+const { createReadStream, createWriteStream } = require("fs")
+const { DEFAULT_HEADER, JPEG_IMAGE_HEADER, PNG_IMAGE_HEADER } = require("./util/util");
 const path = require("path");
 var qs = require("querystring");
 const ejs = require("ejs")
+const parseURL = require("url").parse;
+const formidable = require("formidable");
+
 
 const controller = {
     getHomepage: async (request, response) => {
         let rawData = await fs.readFile("./database/data.json", "utf-8", (err, data) => {
             if (err) console.log(err)
         })
-        jsonData = JSON.parse(rawData)
+        let jsonData = JSON.parse(rawData)
         response.writeHead(200, DEFAULT_HEADER);
         ejs.renderFile("./src/views/index.ejs", { jsonData }, (err, str) => {
             if (err) {
@@ -20,17 +23,17 @@ const controller = {
             }
         })
     },
-    getFormPage: (request, response) => {
+    getFormPage: async (request, response) => {
         return response.end(`
-    <h1>Hello world</h1> <style> h1 {color:red;}</style>
-    <form action="/form" method="post">
-    <input type="text" name="username"><br>
-    <input type="text" name="password"><br>
-    <input type="submit" value="Upload">
-    </form>
-    `);
+        <h1>Hello world</h1> <style> h1 {color:red;}</style>
+        <form action="/form" method="post">
+        <input type="text" name="username"><br>
+        <input type="text" name="password"><br>
+        <input type="submit" value="Upload">
+        </form>
+        `);
     },
-    sendFormData: (request, response) => {
+    sendFormData: async (request, response) => {
         var body = "";
 
         request.on("data", function (data) {
@@ -39,15 +42,59 @@ const controller = {
 
         request.on("end", function () {
             var post = qs.parse(body);
-            console.log(post);
+            console.log(`testing post`);
         });
     },
-    getFeed: (request, response) => {
-        // console.log(request.url); try: http://localhost:3000/feed?username=john123
-        response.write(`    `);
-        response.end();
+    getFeed: async (request, response) => {
+        let rawData = await fs.readFile("./database/data.json", "utf-8", (err, data) => {
+            if (err) console.log(err)
+        })
+        let jsonData = JSON.parse(rawData)
+
+        const { username } = parseURL(request.url, true).query;
+
+        jsonData.forEach(user => {
+            if (user.username === username) {
+                response.writeHead(200, DEFAULT_HEADER);
+                ejs.renderFile("./src/views/user.ejs", { user }, (err, str) => {
+                    if (err) {
+                        console.log(err)
+                    } else {
+                        response.end(str)
+                    }
+                })
+            }
+        });
     },
-    uploadImages: (request, response) => { },
+    uploadImages: async (request, response) => {
+        const { username } = parseURL(request.url, true).query;
+        let rawData = await fs.readFile("./database/data.json", "utf-8", (err, data) => {
+            if (err) console.log(err)
+        })
+        const form = new formidable.IncomingForm();
+        form.parse(request, (err, fields, files) => {
+            if (err) {
+                console.log(err);
+            }
+            filename = files.image.originalFilename;
+            oldPath = files.image.filepath;
+            newPath = path.join(__dirname, "photos", username, filename);
+            createReadStream(oldPath).pipe(createWriteStream(newPath));
+
+            let jsonData = JSON.parse(rawData)
+
+            jsonData.forEach(user => {
+                if (user.username === username) {
+                    user.photos.push(filename)
+                }
+            });
+            mJsonData = JSON.stringify(jsonData)
+            fs.writeFile("./database/data.json", mJsonData, (err) => {
+                if (err) console.log(err)
+                console.log("Photo uploaded")
+            })
+        });
+    },
 };
 
 module.exports = controller;
